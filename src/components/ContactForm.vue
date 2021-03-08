8257d26829c22f81f5695c5f02742c62d99889c3
Audrey Thomasson <audrey@macbook-pro.lan>
2021-03-08 11:53:07 -0600

    first files

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <h3>New Contact</h3>
+      <v-form>
+            <v-text-field 
+                outlined 
+                label="First Name" 
+                v-model="form.firstName">
+            </v-text-field>
+            
+      </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+                firstName: "",
+                lastName: "",
+                phone: "",
+                type: "",
+                email: ""
+            },
+            options: ["Home", "Office", "Cell"]
+        };
+    }
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Phonebook.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello from the phonebook component</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
